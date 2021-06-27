e96c16a984cd6191518d6e5734dee5e53f14e683
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-06-27 15:23:12 +0900

    a

---
+++ kakao.py
@@ -0,0 +1,28 @@
+kakaos = [
+    {
+        "name": "라이언",
+        "gender": "남성",
+        "color": "브라운",
+    },
+    {
+        "name": "무지",
+        "gender": "남성",
+        "color": "화이트",
+    },
+    {
+        "name": "어피치",
+        "gender": "여성",
+        "color": "핑크",
+    },
+    {
+        "name": "콘",
+        "gender": "남성",
+        "color": "그린",
+    },
+]
+
+
+for k in kakaos:
+    print(k["name"])
+    print(k["gender"])
+    print("------")
